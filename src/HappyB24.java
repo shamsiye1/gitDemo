f8064f90d5edce8ea1f3b4c1bfbda34ae541ab27
xamsiye <xamsiye428@gmail.com>
2021-09-25 21:47:58 -0400

    this is my git try

--- src/HappyB24.java
+++ src/HappyB24.java
@@ -3,32 +3,6 @@ public class HappyB24 {
     public static void main(String[] args) {
 
         System.out.println(" java coding is happy, git is existed");
-        System.out.println(
-
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-        );
-
-
-
-
-
-
 
     }
 }
---
+++ src/HappySunday.java
@@ -0,0 +1,9 @@
+public class HappySunday {
+
+    public static void main(String[] args) {
+        System.out.println("i am very happy learn java");
+
+
+
+    }
+}
